57582ef9a1cb25f5bf82e34e5e85fcfe8146c18b
RosiB <blancorosi98@gmail.com>
2022-06-09 16:06:57 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Home />
- 
+      
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import Subscribe from './Subscribe.js';
-
+import Header from './Header.js'
+import Carrusel from './Carrusel.js'
+import Footer from './Footer.js'
 
 export default function Home() {
   return (
     <div className="Home">
+        <Header />
+        <Carrusel /> 
+        
         <Subscribe />
-      
- 
+        <Footer />
     </div>
   );
 }
