07096d9ad81cf02fad492c420ec91c1401ff4bae
Edurne <edurnenun@gmail.com>
2022-06-10 13:48:39 +0200

    econECON-1 cambiado ubicacion experiendeHome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from './components/Home.js';
+import ExperienceHome from "./ExperienceHome.js";
+
 
 function App() {
   return (
     <div className="App">
       <Home />
-      
+      <ExperienceHome />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import Subscribe from './Subscribe.js';
 import Header from './Header.js'
 import Carrusel from './Carrusel.js'
 import Footer from './Footer.js'
-import ExperienceHome from "./ExperienceHome.js";
 
 
 export default function Home() {
