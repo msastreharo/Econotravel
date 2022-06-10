eefdf307b644bdcc0c4d16e93c5d2962b678ee62
Factoria F5 <lauracalderininblue5@gmail.com>
2022-06-10 10:22:44 +0200

    cambios cardc

---
+++ src/Components/CardContact.js
@@ -0,0 +1,34 @@
+
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Melanie from '../assests/melanie.jpg'
+
+function CardContact() {
+    return (
+  
+<div className='containerCard'>
+<div className ="card mb-3" />
+  <div className="row">
+    <div className="col-md-4">
+    <img src={Melanie} className ="rounded float-left"  alt="Melanie" style = {{width: "18rem",marginLeft:"10rem" }} />
+   </div> 
+    <div className="col-md-4" >
+      <div className="card-body">
+        <h5 className="card-title">Recorre cualquier lugar del mundo con Econotravel </h5>
+        <p className="card-text" > La Tarraco Romana, ciudad llena de viejas piedras, 
+         algunas conocidas de antaño, otras aún por descubrir.Cultura viva que nos remonta
+         2000 años atrás.Sea como fuere,una ciudad mágica para perderse y conocer
+         que respira la huella del Imperio Romano y vibra entre nosotros.
+         No te pierdas la experiencia de sentir el pasado .</p>
+         <div><button type="button" className="btn btn-success">Contactanos</button>
+         
+         </div>
+    </div>
+  </div>
+</div>
+</div>
+    
+    )
+}
+ 
+ export default CardContact
