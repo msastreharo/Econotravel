8106e28817513c07eb4b29b9b4abaa93493930b1
Edurne <edurnenun@gmail.com>
2022-06-13 19:56:55 +0200

    db.json #1 Reorganigacion de carpetas en dos principales client para el react y serber para el fake rest api. Añadido dependencias de server auth

---
+++ client/src/components/ExperienceGridCard.js
@@ -0,0 +1,43 @@
+import data from '../dataEx.json'
+import { Row, Col, Card } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+
+const ExperienceGridCard = () =>  {
+  
+    let experienceArray = data;
+  return (
+    experienceArray.map(item =>(
+  
+      <Col key={item.id}>
+        <Card>
+          <Card.Img className='cardImageExper' variant="top" src={item.imagen} />
+          <Card.Body>
+          <Card.ImgOverlay>
+            <Card.Title>{item.nombre}</Card.Title>
+          </Card.ImgOverlay>
+            <Row>
+              <Col><div className='tagCard' ><Card.Text >{item.ubicacion}</Card.Text></div></Col>
+              <Col><div className='tagCard'><Card.Text>{item.transporte}</Card.Text></div></Col>
+              <Col><div className='tagCard'><Card.Text>{item.duracionHoras}</Card.Text></div></Col>
+            </Row>
+                <Card.Text>{item.duracion}</Card.Text>
+              <Row>
+              <Col>
+                <Card.Text>{item.precio}</Card.Text>
+              </Col>
+              <Col>
+                <button className="buttonExp">Reservar</button>
+              </Col>
+              </Row>
+          </Card.Body>
+        </Card>
+      </Col>
+   
+  )))
+}
+
+
+
+
+export default ExperienceGridCard;
+
---
+++ client/src/components/ExperienceHome.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ExperienceNavbar from './ExperienceNavbar.js';
+import ExperienceGridCard from './ExperienceGridCard.js';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Header from './Header.js'
+import Footer from './Footer.js'
+import Ubicacion from "./Filter/category/Ubicacion.js";
+
+
+
+const ExperienceHome = () => (
+  <div className="ExperienceHome">
+    <Header />
+    <div><h1 className="ExperienceTitle">Experiencias en el área de Barcelona</h1></div>
+    <ExperienceNavbar />
+    <Container>
+      <Row xs={1} md={2} lg={3} className="g-4" >
+        <ExperienceGridCard />
+        <Ubicacion />
+      </Row>
+    </Container>
+    <Footer />
+  
+  </div>
+  );
+
+
+
+export default ExperienceHome;
